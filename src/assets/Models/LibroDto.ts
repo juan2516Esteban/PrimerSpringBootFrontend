fe678e62cc94a33934fd515707e98c2c1f183134
Juan Esteban Salazar <juanesteban.salazar1@utp.edu.co>
2023-07-07 23:17:07 -0500

    Lo basico en Angular y sus diferentes eventos

---
+++ src/assets/Models/LibroDto.ts
@@ -0,0 +1,6 @@
+export class LibroDto{
+
+    public idLibro?: number | undefined; // Asepta dos tipos de valores y  posiblemente le vamos a pasar un valor
+    public titulo!: string;
+    public autor!: String;
+}
