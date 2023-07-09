5776d92a5f295ac529faeda25ebba93be0ef6a2d
Juan Esteban Salazar <juanesteban.salazar1@utp.edu.co>
2023-07-09 14:48:38 -0500

    comunicación entre componentes padre a hijo

--- src/app/componentes/hijo/hijo.component.ts
+++ src/app/componentes/hijo/hijo.component.ts
@@ -1,18 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from "@angular/core";
+import { LibroDto } from "src/assets/Models/LibroDto";
 
 @Component({
-  selector: 'app-hijo',
-  templateUrl: './hijo.component.html',
-  styleUrls: ['./hijo.component.css']
+  selector: "app-hijo",
+  templateUrl: "./hijo.component.html",
+  styleUrls: ["./hijo.component.css"]
 })
 export class HijoComponent implements OnInit {
+  constructor() {}
 
-  constructor(){
-
-  }
+  @Input() public recibidoArray: Array<any> = ["estoy vacio"];
+  @Input() public recibido!: String;
+  @Input() public recibidoObjeto!: Array<LibroDto>;
 
   ngOnInit(): void {
-    
+    console.log(this.recibido);
+    console.log(this.recibidoArray);
   }
-
 }
--- src/app/componentes/padre/padre.component.ts
+++ src/app/componentes/padre/padre.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { LibroDto } from "src/assets/Models/LibroDto";
 
 @Component({
-  selector: 'app-padre',
-  templateUrl: './padre.component.html',
-  styleUrls: ['./padre.component.css']
+  selector: "app-padre",
+  templateUrl: "./padre.component.html",
+  styleUrls: ["./padre.component.css"]
 })
-export class PadreComponent {
+export class PadreComponent implements OnInit {
+  constructor() {}
 
+  ngOnInit(): void {
+    this.listaObjetos = [
+      { idLibro: 1, autor: "Juan Esteban Londoño", titulo: "El Trabajador" },
+      { autor: "Nora Elena Londoño", titulo: "La del desafio" }
+    ];
+
+    console.log(this.listaObjetos);
+  }
+
+  public mensajeHijo: String = "Soy tu padre";
+
+  public mensajeArray: Array<any> = [
+    1,
+    2,
+    3,
+    4,
+    "Juan Esteban Salazar",
+    1088825376,
+    "Entro a las 4 a trabajar"
+  ];
+
+  public listaObjetos: Array<LibroDto> = [];
 }
